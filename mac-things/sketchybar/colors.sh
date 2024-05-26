5ef22f5fbf20d19b8383bcaa793f80f2c984ee1b
Anas Hashim <hashim.a@mozn.sa>
2024-05-26 03:46:39 +0300

    mac i3333

---
+++ mac-things/sketchybar/colors.sh
@@ -0,0 +1,9 @@
+#!/bin/bash
+
+export WHITE=0xffffffff
+
+# -- Kyoto Scheme --
+export BAR_COLOR=0x40000000
+export ITEM_BG_COLOR=0xff203f4c
+export ACCENT_COLOR=0xff203f4c
+
---
+++ mac-things/sketchybar/plugins/space_windows.sh
@@ -0,0 +1,24 @@
+#!/bin/bash
+
+echo $INFO
+echo $CONFIG_DIR
+
+if [ "$SENDER" = "space_windows_change" ]; then
+  space="$(echo "$INFO" | jq -r '.space')"
+  apps="$(echo "$INFO" | jq -r '.apps | keys[]')"
+
+  echo $INFO
+  echo $CONFIG_DIR
+
+  icon_strip=" "
+  if [ "${apps}" != "" ]; then
+    while read -r app
+    do
+      icon_strip+=" $($CONFIG_DIR/plugins/icon_map_fn.sh "$app")"
+    done <<< "${apps}"
+  else
+    icon_strip=" —"
+  fi
+
+  sketchybar --set space.$space label="$icon_strip"
+fi
